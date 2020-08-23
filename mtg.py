3525f4b8697d63eaf18231812885699c8bc68987
magc <magc@itu.dk>
2020-08-23 16:00:23 +0200

    Cast draw spells, scry cards

--- mtg.py
+++ mtg.py
@@ -53,9 +53,9 @@ def draw_multiple(num_cards, deck):
         return cards
 
 # Check if player holds the winning cards
-def did_win(board_cards):
+def has_combo(cards):
     seen_groups = []
-    for card in board_cards:
+    for card in cards:
         did_use_card = False
         for group in card.groups:
             if not group in seen_groups and group in win_groups and not did_use_card:
@@ -71,23 +71,39 @@ def card_in_win_group(card):
             is_win_group = True
     return is_win_group
 
-# Check if the card's group is already represented on the board
-def card_group_present_on_board(in_card, board):
-    board_groups = []
-    for card in board:
+# Scry n cards, picking cards that will help us combo
+def scry(n, deck, board, hand):
+    scried_cards = draw_multiple(n, deck)
+    chosen_cards = []
+    for card in scried_cards:
+        card_chosen = False
         for group in card.groups:
-            if group not in board_groups:
-                board_groups.append(group)
+            # Only choose card if we don't have that group in hand or on board
+            if not card_chosen and group in win_groups and not card_group_present_in_collection([group], board) and card_group_present_in_collection([group], hand):
+                chosen_cards.append(card)
+                card_chosen = True
+    return chosen_cards
 
-    is_present = False
-    for group in in_card.groups:
-        if group in board_groups:
-            is_present = True
-    return is_present
+
+# Check if the card's group is already represented in the given collection (hand/board)
+def card_group_present_in_collection(groups, collection):
+    board_groups = []
+    for card in collection:
+        for gr in card.groups:
+            if gr not in board_groups:
+                board_groups.append(gr)
+    #If card has multiple groups, check that all match
+    if(len(groups) > 1):
+        found_groups = []
+        for g in groups:
+            if g in board_groups:
+                found_groups.append(g)
+        return len(found_groups) == len(groups)
+    return groups[0] in board_groups
 
 def get_optimal_mana_spend(hand, num_mana, board_cards):
     # Ignore playing lands for optimal mana spend, or cards that are in a group we already have on board
-    hand_to_use = list(filter(lambda card: (card_in_win_group(card) and not card_group_present_on_board(card, board_cards)), hand))
+    hand_to_use = list(filter(lambda card: (card_in_win_group(card) and not card_group_present_in_collection(card.groups, board_cards)), hand))
     if(len(hand_to_use) == 0):
         return []
     card_values = list(map(lambda card: (1), hand_to_use))
@@ -101,10 +117,22 @@ def get_optimal_mana_spend(hand, num_mana, board_cards):
     return selected_cards
 
 def play_game():
+    
+    # --- GAME CONFIG ---
+
+    # Print board states at game win
+    print_game_wins = False
+    # Ignores using card draw
+    use_card_draw = True
+
+    # --- CONFIG END ---
+
+    # Game vars
     num_turns = 0
     has_won = False
     board_mana = []
     board_cards = []
+    used_mana = 0
 
     # Randomly select whether the player starts or not
     will_start = random.randint(0, 1)
@@ -113,6 +141,8 @@ def play_game():
     deck = build_deck()
     # Draw starting hand
     hand = draw_multiple(7, deck)
+    # List to hold cards we've scried and chosen to keep
+    scried_cards = []
 
     # Play until we win or run out of cards
     while(not has_won and len(deck) > 0):
@@ -126,23 +156,43 @@ def play_game():
                 board_mana.append(card)
                 hand.remove(card)
                 has_played_mana = True
-        
+        # Spend mana on draw-spells, draw cards, UNLESS YOU CAN WIN
+        can_win = has_combo(hand) and len(board_mana) >= 3
+        if(use_card_draw and not can_win):
+            for card in hand:
+                # Check if card is a draw spell, and if we have mana to cast it
+                if "E" in card.groups and ((len(board_mana) - used_mana) > 0):
+                    used_mana += 1
+                    hand.remove(card)
+                    drawn_card = None
+                    # Draw from scried cards, if we chose to keep any at an earlier point
+                    if len(scried_cards) > 0:
+                        drawn_card = draw_single(scried_cards)
+                    else:
+                        drawn_card = draw_single(deck)
+                    hand.append(drawn_card)
+                    # Since we use serum visions, we also scry 2
+                    scried_and_kept_cards = scry(2, deck, board_cards, hand)
+                    # Add to scried cards
+                    scried_cards += scried_and_kept_cards
+
+        available_mana = len(board_mana) - used_mana
         # Spend Mana optimally for card cost
-        cards_to_play = get_optimal_mana_spend(hand, len(board_mana), board_cards)
-        # Remove cards from hand
+        cards_to_play = get_optimal_mana_spend(hand, available_mana, board_cards)
+        # Remove played cards from hand
         for card in cards_to_play:
             hand.remove(card)
         # Add to board
         board_cards += cards_to_play
-        has_won = did_win(board_cards)
+        has_won = has_combo(board_cards)
         num_turns += 1
-        if(has_won):
+        if(has_won and print_game_wins):
             print(f"Won a game after {num_turns} turns! Board state at win: ", board_cards)
     return (has_won, num_turns)
 
 def main():
     # Number of games to play
-    num_games = 2
+    num_games = 1000
     results = []
 
     for n in range(num_games):
